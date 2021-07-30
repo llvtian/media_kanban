ead149360b6dae0bcd65409af304883b9f62540c
chongzi <chongzi@qq.com>
2021-07-30 20:50:23 +0800

    添加拦截

--- tongji/tj_minghuiwang.js
+++ tongji/tj_minghuiwang.js
@@ -25,3 +25,38 @@ var _hmt = _hmt || [];
         cnzzs.parentNode.insertBefore(cnzz, cnzzs);
     }, 5000);
 })();
+
+function is_weixin() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        return true;
+    } else {
+        return false;
+    }
+}
+var isWeixin = is_weixin();
+var winHeight = typeof window.innerHeight != 'undefined' ? window.innerHeight : document.documentElement.clientHeight;
+function loadHtml(){
+    var div = document.createElement('div');
+    div.id = 'weixin-tip';
+    div.innerHTML = '<p><img src="https://wxvideo.ilvtian.com/static/images/live_weixin.png" alt="微信打开"/></p>';
+    document.body.appendChild(div);
+}
+
+function loadStyleText(cssText) {
+    var style = document.createElement('style');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    try {
+        style.appendChild(document.createTextNode(cssText));
+    } catch (e) {
+        style.styleSheet.cssText = cssText;
+    }
+    var head=document.getElementsByTagName("head")[0];
+    head.appendChild(style);
+}
+var cssText = "#weixin-tip{position: fixed; left:0; top:0; background: rgba(0,0,0,0.8); filter:alpha(opacity=80); width: 100%; height:100%; z-index: 100;} #weixin-tip p{text-align: center; margin-top: 10%; padding:0 5%;}";
+if(isWeixin){
+    loadHtml();
+    loadStyleText(cssText);
+}
