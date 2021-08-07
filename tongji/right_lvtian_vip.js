80bbe0e47d368218c93616ff974282192e383ebd
chongzi <chongzi@qq.com>
2021-08-07 10:25:37 +0800

    更改样式

--- tongji/right_lvtian_vip.js
+++ tongji/right_lvtian_vip.js
@@ -59,7 +59,7 @@ if(/android|webos|iphone|ipod|ipad/i.test(cfua)) {
     // sjdb_divwidth="100%";
     sjdb_divwidth="30%";
 }else{
-    sjdb_divwidth="20%";
+    sjdb_divwidth="200px";
     // sjdb_divwidth=sjdb_adwidth+"px";
     // if(sjdb_adwidth>sjdb_w){sjdb_leftpx="0px";}else{sjdb_leftpx=((sjdb_w - sjdb_adwidth) / 2)+"px";}
 }
